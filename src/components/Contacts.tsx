3778f340afc3c2c4160b0882755288d9470a1608
Wojtek Dudek <wdudek82@gmail.com>
2018-08-05 13:50:29 +0200

    Moved rendering contacts as Contact components; disabled sorting alphabetically object keys

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -3,25 +3,44 @@ import Contact from './Contact';
 
 import styledComponents from 'styled-components';
 
+// ------- styles
 const Wrapper = styledComponents.div.attrs({
   className: 'container',
 })``;
 
-const Contacts = (props: any) => {
-  return (
-    <Wrapper>
-      <Contact
-        name="Jaś Fasola"
-        email="jfasola@example.com"
-        phone="555-555-5555"
-      />
-      <Contact
-        name="Karen Smith"
-        email="ksmith@example.com"
-        phone="555-555-5555"
-      />
-    </Wrapper>
-  );
-};
+class Contacts extends React.Component<{}, {}> {
+  public state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jaś Fasola',
+        email: 'jfasola@example.com',
+        phone: '555-555-5555',
+      },
+      {
+        id: 2,
+        name: 'Karen Smith',
+        email: 'ksmith@example.com',
+        phone: '123-345-5678',
+      },
+      {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'hjohnson@gmail.com',
+        phone: '877-636-1353',
+      },
+    ],
+  };
+
+  public renderContacts() {
+    return this.state.contacts.map(({ id, name, email, phone }) => (
+      <Contact key={id} name={name} email={email} phone={phone} />
+    ));
+  }
+
+  public render() {
+    return <Wrapper>{this.renderContacts()}</Wrapper>;
+  }
+}
 
 export default Contacts;
